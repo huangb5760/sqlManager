a61ffb5aee6e68703556b4c4770ea970a1e8aaa9
murph@damao <murphyl@outlook.com>
2022-02-11 14:30:16 +0800

    JSONEditor

---
+++ site/src/plug/components/form-item/textarea/textarea.module.jsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames';
+import kindOf from 'kind-of';
+
+import FormItem from '../base/form-item-base.module';
+
+import styles from './textarea.module.css';
+
+export default function TextArea({ className, inline, onChange }) {
+    const valueChanged = (value) => {
+        if (kindOf(onChange) === 'function') {
+            onChange(value);
+        }
+    };
+    return (
+        <FormItem className={classNames(styles.root, className)} inline={inline}>
+            <textarea rows="2" onChange={({ target }) => valueChanged(target.value)} />
+        </FormItem>
+    );
+}
